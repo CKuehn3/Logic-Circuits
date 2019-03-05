78c4356d680772c46631a66450259d3a151f94e2
Justin Scyphers <Justin@Justins-MacBook-Pro-2.local>
2019-03-05 09:20:30 -0500

    Added Justin Folder

---
+++ src/Justin/Main Menu 2/Assets/Scripts/SelectOnInput.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class SelectOnInput : MonoBehaviour
+{
+
+    public EventSystem evSys;
+    public GameObject selectedObj;
+    private bool buttonSelected;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw ("Vertical") != 0 && buttonSelected == false)
+        {
+            evSys.SetSelectedGameObject(selectedObj);
+            buttonSelected = true;
+        }
+    }
+
+    private void OnDisable()
+    {
+        buttonSelected = false;
+    }
+}
