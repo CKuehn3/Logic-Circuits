1c83fb66f7db09ab2bd635d2e62f1987be5b5eb5
Chase Kuehn <ckuehn@radford.edu>
2019-03-31 14:20:39 -0400

    Added Clear Script

---
+++ src/Justin/Main Menu 2/Assets/UI/Scripts/Undo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Undo : MonoBehaviour
+{
+    [SerializeField] private Button btn;
+    private void Start()
+    {
+        btn.onClick.AddListener(Clear);
+    }
+
+
+    // Update is called once per frame
+    void Clear()
+    {
+
+        GameObject[] allGates = GameObject.FindGameObjectsWithTag("Gates");
+
+        foreach(GameObject gate in allGates)
+        {
+            Destroy(gate);
+        }
+    }
+}
