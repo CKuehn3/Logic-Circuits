cfe39c11bc783075ee94de926ab21d0b379f6258
Chase Kuehn <ckuehn@radford.edu>
2019-03-31 15:00:40 -0400

    Added Linerenderer stuff

---
+++ src/Justin/Main Menu 2/Assets/UI/Scripts/Undo1.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Undo1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private Button btn;
+    private Stack<GameObject> undStk = new Stack<GameObject>();
+    void Start()
+    {
+        btn.onClick.AddListener(undo);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject[] allGates = GameObject.FindGameObjectsWithTag("Gates");
+       // Debug.Log(allGates.Length);
+        if(allGates.Length != 0)
+        {
+            for(int i = 0; i < allGates.Length; i++)
+            {
+                undStk.Push(allGates[i]);
+            }
+            
+
+        }
+        
+
+    }
+    void undo()
+    {
+        if (undStk.Count == 0)
+        {
+            Debug.Log("Can't pop off empty stack");
+        }
+        else
+        {
+            GameObject obj = undStk.Pop();
+            Destroy(obj);
+        }
+    }
+}
---
+++ src/LineRenderer/LineRendererTest/Assets/DrawLines.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLines : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject lineGeneratorPrefab;
+    [SerializeField]
+    private GameObject linePointPrefab;
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            newPos.z = 0;
+
+            CreatePointMarker(newPos);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            ClearAllPoints();
+        }
+        if (Input.GetKeyDown("e"))
+        {
+            GenerateNewLine();
+        }
+    }
+
+    private void CreatePointMarker(Vector3 pointPosition)
+    {
+        Instantiate(linePointPrefab, pointPosition, Quaternion.identity);
+    }
+
+    private void ClearAllPoints()
+    {
+        GameObject[] allPoints = GameObject.FindGameObjectsWithTag("PointMarker");
+        
+        foreach (GameObject point in allPoints)
+        {
+            Destroy(point);
+        }
+    }
+
+    private void GenerateNewLine()
+    {
+        GameObject[] allPoints = GameObject.FindGameObjectsWithTag("PointMarker");
+        Vector3[] allPointPositions = new Vector3[allPoints.Length];
+
+        if(allPoints.Length >= 2)
+        {
+            for (int i = 0; i < allPoints.Length; i++)
+            {
+                allPointPositions[i] = allPoints[i].transform.position;
+            }
+            SpawnLineGenerator(allPointPositions);
+        }
+        else
+        {
+            Debug.Log("Need 2 or more points to create a line.");
+        }
+    }
+    private void SpawnLineGenerator(Vector3[] linePoints)
+    {
+        GameObject newLineGen = Instantiate(lineGeneratorPrefab);
+        LineRenderer lrend = newLineGen.GetComponent<LineRenderer>();
+
+        lrend.positionCount = linePoints.Length;
+        lrend.SetPositions(linePoints);
+        
+        Destroy(newLineGen, 5);
+    }
+
+}
