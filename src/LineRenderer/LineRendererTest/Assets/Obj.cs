dca06517a2b8250b2b3cd728a1b7c92a3bccbe84
jmscyphers@gmail.com <jmscyphers@gmail.com>
2019-04-01 00:14:36 -0400

    Update 2: Electric Boogaloo

---
+++ src/LineRenderer/LineRendererTest/Assets/Obj.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obj : MonoBehaviour
+{
+    private bool input;
+    private bool output;
+    // Start is called before the first frame update
+    void Start()
+    {
+        input = false;
+        output = true;
+    }
+
+    void setInput()
+    { 
+        if (input == false)
+        {
+            input = true;
+        }
+    }
+
+    void setOut()
+    {
+        if (output == false)
+        {
+            output = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
