df59f75431272ba88ebc8333f3b31d5597a7d527
Jessica Archer <jarcher7@radford.edu>
2019-04-01 14:09:38 -0400

    Build drag and drop functionality for spawned prefabs.

---
+++ src/Justin/Main Menu 2/Assets/DragHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems; 
+
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public static GameObject item;
+    Vector2 startPosition;
+    Transform startParent; 
+
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        item = gameObject;
+        startPosition = transform.position;
+        startParent = transform.parent;
+       // GetComponent<CanvasGroup>().blocksRaycasts = false; 
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //transform.position = Input.mousePosition; 
+        var screenPoint = (Vector3)Input.mousePosition;
+        screenPoint.z = 10.0f; //distance of the plane from the camera
+        transform.position = Camera.main.ScreenToWorldPoint(screenPoint);
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        item = null;
+       //GetComponent<CanvasGroup>().blocksRaycasts = true;
+        if (transform.parent != startParent)
+        {
+            transform.position = startPosition;
+        }
+    }
+
+}
