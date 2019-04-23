d6f82c5b82b8d45501e27bf5804c7b91663b7c4f
crebtheirken <cfuhrer@radford.edu>
2019-04-23 00:23:41 -0400

    Tablebehavior edit
    Change Tablebehavior to change XYZ variables and pull it's result from the result item.

--- src/Justin/Main Menu 2/Assets/UI/Scripts/TableBehavior.cs
+++ src/Justin/Main Menu 2/Assets/UI/Scripts/TableBehavior.cs
@@ -12,14 +12,17 @@ public class TableBehavior : MonoBehaviour
         string[,] items = new string[,]
         {
             {"X","Y","Z","A"},
-            {"T","T","T", "T"},
-            {"T","T","F", "T"},
-            {"F","F","T", "F"},
-            {"F","T","F", "F"},
-            {"F","F","F", "T"}
+            {"F","F","F", "T"},
+            {"F","F","T", "T"},
+            {"F","T","F", "T"},
+            {"F","T","T", "F"},
+            {"T","F","F", "F"},
+            {"T","F","T", "F"},
+            {"T","T","F", "F"},
+            {"T","T","T", "F"}
         };
         int Variables = 3;
-        int Rows = 5;
+        int Rows = 8;
         GameObject Header = GameObject.Find("TableHead");
         GameObject HeaderItem = GameObject.Find("TableItem(0)");
         GameObject TableArea = GameObject.Find("TableData");
@@ -63,24 +66,44 @@ public class TableBehavior : MonoBehaviour
     }
     
     public void Check() {
-        int Rows = 5;
+        int Rows = 8;
         int Variable = 3;
+        GameObject inputx = GameObject.Find("Input X");
+        GameObject inputy = GameObject.Find("Input Y");
+        GameObject inputz = GameObject.Find("Input Z");
+        GameObject result = GameObject.Find("Result");
         for (int i = 0; i < (Rows); i++)
         {
             GameObject Current = GameObject.Find("TruthTable" +
                 "/TableData/TableDataItem(" + i + ")");
-            Debug.Log("Current: " + Current); 
-            GameObject Text = GameObject.Find("TruthTable" +
+            Debug.Log("Current: " + Current);
+            GameObject Xval = GameObject.Find("TruthTable" +
+                "/TableData/TableDataItem(" + i + ")" +
+                "/ToF(" + 0 + ")");
+            GameObject Yval = GameObject.Find("TruthTable" +
+                "/TableData/TableDataItem(" + i + ")" +
+                "/ToF(" + 1 + ")");
+            GameObject Zval = GameObject.Find("TruthTable" +
+                "/TableData/TableDataItem(" + i + ")" +
+                "/ToF(" + 2 + ")");
+            GameObject resultText = GameObject.Find("TruthTable" +
                 "/TableData/TableDataItem(" + i + ")" +
                 "/ToF(" + Variable + ")");
 
-            if (StringtoBool(Text.GetComponent<Text>().text))// == vairable location
-            {
-                Current.GetComponent<Image>().color = new Color32(0, 255, 0, 50);
-            }
-            else { Current.GetComponent<Image>().color = new Color32(255, 0, 0, 50); }
-    
-    
+            inputx.GetComponent<TestPositiveInput>().setValue(StringtoBool(Xval.GetComponent<Text>().text));
+            Debug.Log("X value Set to " + StringtoBool(Xval.GetComponent<Text>().text));
+            inputy.GetComponent<TestPositiveInput>().setValue(StringtoBool(Yval.GetComponent<Text>().text));
+            Debug.Log("Y value Set to " + StringtoBool(Yval.GetComponent<Text>().text));
+            inputz.GetComponent<TestPositiveInput>().setValue(StringtoBool(Zval.GetComponent<Text>().text));
+            Debug.Log("Z value Set to " + StringtoBool(Zval.GetComponent<Text>().text));
+
+            if (StringtoBool(resultText.GetComponent<Text>().text) == result.GetComponent<Result>().value)
+                {
+                    Current.GetComponent<Image>().color = new Color32(0, 255, 0, 50);
+                Debug.Log(resultText.GetComponent<Text>().text + " = " + result.GetComponent<Result>().value);
+                }
+                else { Current.GetComponent<Image>().color = new Color32(255, 0, 0, 50); Debug.Log(resultText.GetComponent<Text>().text + " != " + result.GetComponent<Result>().value); }
+                
         }
     }
 
